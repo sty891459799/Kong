fbd43ff4a037a237798b7253a93beb588034d588
test <sty891459799@163.com>
2020-09-14 10:27:27 +0800

    0.1.0

---
+++ examples/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <K-input Plr="Blue sky, good Life -- Kong" color="#DF6589" width="500px"></K-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ packages/Input/src/Input.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="k-Inp" :style="{width:this.width}">
+    <input type="text" :placeholder="Plr" :style="{color:this.Color,borderColor:this.Color}" />
+    <label class="input-dom"></label>
+  </div>
+</template>
+<script>
+export default {
+  name: "K-input",
+  props: ["Plr", "Color", "width"],
+  created() {},
+};
+</script>
+
+<style scoped>
+#k-Inp {
+  position: relative;
+  max-width: 500px;
+  min-width: 150px;
+  height: 50px;
+}
+input {
+  outline: none;
+  border: none;
+  padding: 5px 10px;
+  z-index: 99;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+  top: 50%;
+  text-align: center;
+  margin-top: -15px;
+  width: 73%;
+}
+.input-dom::after,
+.input-dom::before {
+  content: "";
+  position: absolute;
+  right: 0;
+  width: 50%;
+  height: 150%;
+  z-index: 9;
+  height: 40px;
+  border: 4px solid #eee;
+}
+.input-dom::after {
+  border-left: none;
+}
+.input-dom::before {
+  left: 0;
+  border-right: none;
+}
+input:focus,
+.input-dom::before,
+.input-dom::after {
+  transition: ease 0.3s;
+}
+input:focus + .input-dom::before {
+  transform: translate3d(-10%, 0, 0);
+}
+input:focus + .input-dom::after {
+  transform: translate3d(10%, 0, 0);
+}
+</style>
